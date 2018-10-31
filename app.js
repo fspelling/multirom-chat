01bac195410f812d02f9974a6810e4e82ea7a00e
FERNANDO LENTISCO SPELLING <fernandpel@cptm.info>
2018-10-31 09:38:28 -0300

    .

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var app = require('./config/server');
 var socket = require('socket.io');
 
-var server = app.listen(80, function(){
+var server = app.listen(4000, function(){
     console.log('servidor rodando');
 });
 
 var io = socket.listen(server);
 
-io.on('connection', function(con){
-    console.log('servidor rodando');
+io.on('connection', function(conSocket){
+    console.log('servidor socket rodando');
+
+    conSocket.on('disconnect', function(){
+        console.log('servidor socket fora');
+    });
 });
